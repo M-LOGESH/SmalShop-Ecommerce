7d297b4abf08e828281905c7d16538516545c743
M-LOGESH <logeshm724@gmail.com>
2025-09-27 22:38:25 +0530

    fix: myaccount app added to admin page

--- backend/myaccount/admin.py
+++ backend/myaccount/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "full_name","user__email", "dob", "gender", "mobile","address")
+    search_fields = ("user__username", "full_name", "mobile")
