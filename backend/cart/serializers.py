2c5c5121c58b989dba76c104c5e914c4bf5265b1
M-LOGESH <logeshm724@gmail.com>
2025-10-03 00:21:30 +0530

    fix: minor change in cart

--- backend/cart/serializers.py
+++ backend/cart/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from .models import Wishlist, Cart
+from products.serializers import ProductSerializer
+from products.models import Product
 
 class WishlistSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')  # optional
+    user = serializers.ReadOnlyField(source='user.username')
+    # writable ID for POST/PUT
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    # read-only nested data for GET
+    product_detail = ProductSerializer(source='product', read_only=True)
+
     class Meta:
         model = Wishlist
-        fields = ["id", "product", "user"]
+        fields = ["id", "product", "product_detail", "user"]
 
 
 class CartSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')  # optional
+    user = serializers.ReadOnlyField(source='user.username')
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    product_detail = ProductSerializer(source='product', read_only=True)
+
     class Meta:
         model = Cart
-        fields = ["id", "product", "user", "quantity"]
-
+        fields = ["id", "product", "product_detail", "user", "quantity"]
