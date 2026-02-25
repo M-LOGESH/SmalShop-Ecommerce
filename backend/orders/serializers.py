4665b2b94b4eda86c5a73fb9b3ee9b355c58f151
M-LOGESH <logeshm724@gmail.com>
2026-02-25 14:41:52 +0530

    fix: product pagination fixed

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from .models import Order, OrderItem
-from products.serializers import ProductSerializer
+from products.serializers import ProductListSerializer
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product_detail = ProductSerializer(source="product", read_only=True)
+    product_detail = ProductListSerializer(source="product", read_only=True)
 
     class Meta:
         model = OrderItem
